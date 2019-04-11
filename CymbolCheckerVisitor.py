e8b3bac4a9b9666329339d81d2a6f0a30e86256b
Mateus Valgueiro <mvalgueiro@apple.com>
2019-04-10 23:33:47 -0300

    add expr operations and refine .gitignore

--- CymbolCheckerVisitor.py
+++ CymbolCheckerVisitor.py
@@ -18,22 +18,26 @@ class CymbolCheckerVisitor(CymbolVisitor):
         value = int(ctx.getText())
         return {'type': Type.INT, 'value': value}
 
-    def visitStringExpr(self, ctx: CymbolParser.IntExprContext):
+    def visitStringExpr(self, ctx: CymbolParser.StringExprContext):
         value = ctx.getText()
-        return {'type': Type.String, 'value': value}
+        return {'type': Type.STRING, 'value': value}
 
     def visitFloatExpr(self, ctx: CymbolParser.FloatExprContext):
         value = float(ctx.getText())
         return {'type': Type.FLOAT, 'value': value}
 
-    def visitBooleanExpr(self, ctx: CymbolParser.FloatExprContext):
+    def visitBooleanExpr(self, ctx: CymbolParser.BooleanExprContext):
         value = bool(ctx.getText())
         return {'type': Type.BOOLEAN, 'value': value}
+
+    def visitVarIdExpr(self, ctx: CymbolParser.VarIdExprContext):
+        var_name = ctx.ID.getText()
+        return self.id_values[var_name]
+
 
     def visitVarDecl(self, ctx: CymbolParser.VarDeclContext):
         var_name = ctx.ID().getText()
         tyype = ctx.tyype().getText()
-        print("tyype = " + tyype)
 
         if (tyype == Type.VOID):
             result = Type.VOID
@@ -41,30 +45,40 @@ class CymbolCheckerVisitor(CymbolVisitor):
             exit(1)
         else:
             if ctx.expr() != None:
-                init = ctx.expr().accept(self)
-                print("init = " + init["type"])
-                if init != tyype:
+                result = self.visit(ctx.expr())
+                if result['type'] != tyype:
                     print("Mensagem de erro 2...")
                     exit(2)
+                out = {'type': tyype, 'value': result['value']}
+                self.id_values[var_name] = tyype
 
-            result = tyype
-            self.id_values[var_name] = tyype
+        return out
 
-        print("saved variable " + var_name + " of type " + tyype)
-        return result
+    def visitSignedExpr(self, ctx: CymbolParser.SignedExprContext):
+        expr = self.visit(ctx.expr)
+        op = ctx.op.text
 
-	def visitAddSubExpr(self, ctx: CymbolParser.AddSubExprContext):
+        if expr['type'] in [Type.INT, Type.FLOAT]:
+            value = expr['value']
+            if op == '-':
+                value *= -1
+            return {'type': expr['type'], 'value': value}
+        return 0
+
+    def visitNotExpr(self, ctx: CymbolParser.NotExprContext):
+        expr = self.visit(ctx.expr)
+
+        if expr['type'] == Type.BOOLEAN:
+            return {'type': Type.BOOLEAN, 'value': not expr['value']}
+        return 0
+
+    def visitComparisonExpr(self, ctx: CymbolParser.ComparisonExprContext):
         left = self.visit(ctx.expr()[0])
         right = self.visit(ctx.expr()[1])
         op = ctx.op.text
 
-        if op == '+':
-            result = left["value"] + right["value"]
-        else:
-            result = left["value"] - right["value"]
-
-        print(result)
-        return {'type': Type.INT, 'value': result}
+        result = eval(str(left["value"]) + op + str(right["value"]))
+        return {'type': Type.BOOLEAN, 'value': result}
 
     def visitMulDivExpr(self, ctx: CymbolParser.MulDivExprContext):
         left = self.visit(ctx.expr()[0])
@@ -78,26 +92,50 @@ class CymbolCheckerVisitor(CymbolVisitor):
 
         print(result)
         return {'type': Type.INT, 'value': result}
-	
-	def visitComparisonExpr(self, ctx: CymbolParser.ComparisonExprContext):
+
+    def visitAddSubExpr(self, ctx: CymbolParser.AddSubExprContext):
         left = self.visit(ctx.expr()[0])
         right = self.visit(ctx.expr()[1])
         op = ctx.op.text
 
-        result = eval(str(left["value"]) + op + str(right["value"]))
-        return {'type': Type.BOOLEAN, 'value': result}
-	
-	def visitEqExpr(self, ctx:CymbolParser.BoolExprContext):
+        if op == '+':
+            result = left["value"] + right["value"]
+        else:
+            result = left["value"] - right["value"]
+
+        print(result)
+        return {'type': Type.INT, 'value': result}
+
+    def visitEqExpr(self, ctx: CymbolParser.EqExprContext):
         left = self.visit(ctx.expr()[0])
-		right = self.visit(ctx.expr()[1])
-		op = ctx.op.text
+        right = self.visit(ctx.expr()[1])
+        op = ctx.op.text
 
-		if op == '==':
-			result =  (left["value"] == right["value"])
-		else:
-			result = (left["value"] != right["value"])
+        if op == '==':
+            result = (left["value"] == right["value"])
+        else:
+            result = (left["value"] != right["value"])
 
-		return {'type': Type.BOOLEAN, 'value': result}
+        return {'type': Type.BOOLEAN, 'value': result}
+
+    def visitParenExpr(self, ctx: CymbolParser.ParenExprContext):
+        return self.visit(ctx.expr())   
+    
+    def visitBoolExpr(self, ctx: CymbolParser.BoolExprContext):
+        left = self.visit(ctx.expr()[0])
+        right = self.visit(ctx.expr()[1])
+        op = ctx.op.text
+
+        if left['type'] == Type.BOOLEAN and right['type'] == Type.BOOLEAN:
+            if op == '&&':
+                result = left['value'] and right ['value']
+            else:
+                result = left['value'] or right ['value']
+            return {'type': Type.BOOLEAN, 'value': result}
+        return 0
 
     def aggregateResult(self, aggregate: Type, next_result: Type):
         return next_result if next_result != None else aggregate
+
+
+
